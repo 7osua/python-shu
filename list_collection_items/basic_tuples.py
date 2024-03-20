5b49a0bb973497de7f5c37b5efc72022146fa409
7osua <j7.manullang@gmail.com>
2024-03-21 03:22:14 +0700

    [+] learn about tuples

---
+++ list_collection_items/basic_tuples.py
@@ -0,0 +1,15 @@
+'''
+link : https://www.pythontutorial.net/python-basics/python-tuples/
+------------------------------------------------------------------
+Learn about Python tuples and how to use them effectively.
+A tuple is a list that cannot change. Python refers to a value that cannot change as immutable.
+So by definition, a tuple is an immutable list.
+'''
+
+rgb = ("red", "green", "blue")
+
+print(rgb[0])
+print(rgb[1])
+print(rgb[2])
+
+print(type(rgb))
