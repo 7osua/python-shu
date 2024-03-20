2d214fc5319a677edf1e19846b5fd7414c3be86a
7osua <j7.manullang@gmail.com>
2024-03-21 04:14:01 +0700

    [+] learn tupples.

--- list_collection_items/basic_tuples.py
+++ list_collection_items/basic_tuples.py
@@ -6,10 +6,23 @@ A tuple is a list that cannot change. Python refers to a value that cannot chang
 So by definition, a tuple is an immutable list.
 '''
 
+# Defining a Tuple.
 rgb = ("red", "green", "blue")
 
 print(rgb[0])
 print(rgb[1])
 print(rgb[2])
 
-print(type(rgb))
+# Defining a tuple that has one element.
+numbers = (3,)
+print(type(numbers))
+
+numbers = (3)
+print(type(numbers))
+
+# Assigning a tuple
+days = ("Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday")
+print(days)
+
+days = ("Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday")
+print(days)
