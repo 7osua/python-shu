601c4efec0847b81a40e187140bee89ac43e8306
7osua <j7.manullang@gmail.com>
2024-03-25 12:26:58 +0700

    [^] Using map function on a list of tuples. To assign new value on every item in a tuples

--- list_collection_items/list_map_functions_py.py
+++ list_collection_items/list_map_functions_py.py
@@ -3,6 +3,8 @@ link : https://www.pythontutorial.net/python-basics/python-map-list/
 ---------------------------------------------------------------------
 Learn how to use the Python map() function with lists.
 When working with a list (or a tuple), you often need to transform the elements of the list and return a new list that contains the transformed element.
+
+- Use the Python map() to call a function on every item of a list and returns an iterator.
 '''
 
 bonuses = [100, 200, 300]
@@ -35,3 +37,17 @@ new_names = map(lambda name: name.upper(), names)
 
 print(names)
 print(list(new_names))
+
+# Using the map() function to a list of tuples
+
+'''Suppose that you have the following shopping cart represented as a list of tuples'''
+carts = [['Smartphones', 400],
+         ['Tablet', 450],
+         ['Laptop', 700]]
+
+'''And you need to calculate the tax amount for each product with a 10% tax'''
+
+tax = 10/100
+print(f'tax = {tax}')
+carts = map(lambda item: [item[0], item[1], item[1] * tax], carts)
+print(list(carts))
