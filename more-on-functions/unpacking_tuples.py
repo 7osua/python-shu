fee9c1be1028b72ad196798d595293b71e119118
7osua <j7.manullang@gmail.com>
2024-05-25 04:30:49 +0700

    [^] start learning more on fucntions

---
+++ more-on-functions/unpacking_tuples.py
@@ -0,0 +1,18 @@
+# link : https://www.pythontutorial.net/python-basics/python-unpacking-tuple/
+
+"""
+Learn how to un-tuples.
+"""
+
+# Review Tuples "()"
+
+"""
+- Python using commas" val1,val2" to define tuples, not parantheses().
+- Python using parantheses "(val1, val2)", so to make the tuple more clearer.
+- Python using empty parantheses (), to define an empty tuples.
+"""
+
+a_tuple = 1, 2
+a_tuple_parantheses = (3, 4)
+print(type(a_tuple))
+print(type(a_tuple_parantheses))
