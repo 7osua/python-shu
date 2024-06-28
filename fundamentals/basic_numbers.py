6ac281b3407cba51569e9e69bb645dd027ae1956
7osua <j7.manullang@gmail.com>
2024-06-28 19:16:44 +0700

    [^] Learn about numbers, understanding how numbers works in python and using the underscores to make code readable by grouping the digit

--- fundamentals/basic_numbers.py
+++ fundamentals/basic_numbers.py
@@ -1,33 +1,110 @@
-'''
+"""
 link : https://www.pythontutorial.net/python-basics/python-numbers/
---------------------------------------------------------------------
-Learn about Python numbers and how to use them in programs.
-'''
+"""
 
-sum = 2 + 10
+# Integers
+
+sum = 20 + 10
 print(sum)
-sub = sum - 2
-print(sum)
-mul = sub * 2
+# [Output] 30
+
+sub = 20 - 10
+print(sub)
+# [Output] 10
+
+mul = 20 * 10
 print(mul)
-div = mul / 2
+# [Output] 200
+
+div = 20 / 10
 print(div)
+# [Output] 2.0
 
-# exponent operator
-print(2 ** 3)
-print(3 ** 3)
+exp = 3**3  # Calculate exponents with '**'
+print(exp)  # With two multiplication symbols
+# [Output] 27
 
-# orders of operatorions
-print(2 / (1 + 1))
+ord = 20 / (10 + 10)  # Order of operations, with the '()' parantheses.
+print(ord)
+# [Output] 1.0
+
+"""
+The integers are number such as -1, 0, 1, 2, and 3, ...
+and they have type 'int'.
+
+You can use Math operator like '+', '-', '*', and '/'
+to form expressions that include integers.
+"""
+
+# Floats
+
+"""
+Any number with decimal point '.' is a floating-point number.
+The term float means that decimal point can appear at any
+posisiton in a number.
+
+In general we can use float like intergers.
+"""
 
 print(0.5 + 0.5)
-print(0.5 - 0.5)
-print(0.5 * 0.5)
-print(0.5 / 0.5) # division always return a float
-print(1 + 2.0) # if float type is used with integer number will return floating point number
-print(0.1 + 0.2)
+# [Output] 1.0
 
+print(0.5 - 0.5)
+# [Output] 0.0
+
+print(0.5 * 0.5)
+# [Output] 0.25
+
+
+print(0.5 / 0.5)  # division always return a float
+# [Output]
+
+"""
+If you mix an integer and a float in any arithmetic operation,
+the result is a float.
+"""
+print(1 + 2.0)
+# [Output] 3.0
+
+
+"""
+[Note]
+Due to the internal representation of floats, Python will try to
+represent the result as precisely as possible. However, you may
+get the result that you would not expect.
+
+Just keep this in mind when you perform calculations with floats.
+"""
+print(0.1 + 0.2)
+# [Output] 0.30000000000000004
+
+
+# Underscores in numbers
+
+"""
+When a number is large,  it'll becoame difficult to read.
+
+    count = 10000000000
+
+To make the long numbers more readable, you can group digits
+using underscores.
+The underscores also work for both the integers and floats.
+"""
 count = 10_000_000_000
 print(count)
-tax = 1.2_000_1009
+# [Output] 10000000000
+
+tax = 1.2_000_10090
 print(tax)
+# [Output] 1.200010090
+
+"""
+Learn about Python numbers and how to use them in programs.
+
+[Summary]
+
+- Python supports common numeric types including
+  intergers, floats, and complex numbers.
+- Use the underscores to group numbers for the large
+  numbers.
+"""
